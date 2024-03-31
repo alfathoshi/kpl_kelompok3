fc8b972c6578bbbb6ec4a13bdc192c983345ca69
ArgaAAL <109477340+ArgaAAL@users.noreply.github.com>
2024-03-31 18:59:30 +0700

    Fix Program.cs
    Lupa edit balik

--- tpmodul7_kelompok_3/Program.cs
+++ tpmodul7_kelompok_3/Program.cs
@@ -1,18 +1,7 @@
-﻿using System;
-
-namespace tpmodul7_kelompok_3
+internal class Program
 {
-    class Program
+    private static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            var dataMahasiswa = new DataMahasiswa1302223038();
-            dataMahasiswa.ReadJSON();
-
-            var kuliahMahasiswa = new KuliahMahasiswa1302223038();
-            kuliahMahasiswa.ReadJSON();
-
-            Console.ReadLine(); // Tahan tampilan konsol
-        }
+        Console.WriteLine("Hello, World!");
     }
 }
