e9dd3445ca4f7bdc86d309d37078b87e0a04fd50
wildanniam <wildanniam4@gmail.com>
2024-03-31 22:09:55 +0700

    menambahkan class DataMahasiswa1302220005

---
+++ tpmodul7_kelompok_3/DataMahasiswa1302220005.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace tpmodul7_kelompok_3
+{
+    internal class DataMahasiswa1302220005
+    {
+        public class NamaMahasiswa
+        {
+            public string depan { get; set; }
+            public string belakang { get; set; }
+        }
+        public NamaMahasiswa nama { get; set; }
+        public long nim { get; set; }
+        public string fakultas { get; set; }
+
+        public void ReadJSON()
+        {
+            string json = File.ReadAllText("tp7_1_1302220005.json");
+
+            var dataMahasiswa = JsonSerializer.Deserialize<DataMahasiswa1302220005>(json);
+
+            if (dataMahasiswa.nama != null)
+            {
+                Console.WriteLine($"Nama {dataMahasiswa.nama.depan} {dataMahasiswa.nama.belakang} dengan nim {dataMahasiswa.nim} dari fakultas {dataMahasiswa.fakultas}");
+            }
+            else
+            {
+                Console.WriteLine("Nama mahasiswa tidak ada.");
+            }
+        }
+    }
+}
