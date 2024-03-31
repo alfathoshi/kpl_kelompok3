57f07f5819ba991096cb9fbcf2586ec11952e5fd
unknown <farisazhar.0241@gmail.com>
2024-03-31 23:45:09 +0700

    Menambahkan Class DataMahasiswa_1302223123

---
+++ tpmodul7_kelompok_3/KuliahMahasiswa1302223123.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+public class KuliahMahasiswa1302223123
+{
+ 
+    
+}
--- tpmodul7_kelompok_3/Program.cs
+++ tpmodul7_kelompok_3/Program.cs
@@ -5,20 +5,12 @@ namespace tpmodul7_kelompok_3
     class Program
     {
         static void Main(string[] args)
-        {
-            var dataMahasiswa = new DataMahasiswa1302220005();
-            dataMahasiswa.ReadJSON();
+        { 
+            DataMahasiswa1302223123 dataMhs = new DataMahasiswa1302223123();
 
-            var kuliahMahasiswa = new KuliahMahasiswa1302220005();
-            kuliahMahasiswa.ReadJSON();
-
-            Console.ReadLine(); 
-  
-            DataMahasiswa1302223099 dataMhs = new DataMahasiswa1302223099();
-            KuliahMahasiswa1302223099 KuliahMhs = new KuliahMahasiswa1302223099();
             dataMhs.ReadJSON();
             Console.WriteLine();
-            KuliahMhs.ReadJSON();
+            
         }
 
     }
